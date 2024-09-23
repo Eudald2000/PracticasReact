31b156b3a00cc2f8a2e8b58e83b29e8bde661906
Eudald2000 <eudald08210@gmail.com>
2024-09-23 19:21:09 +0200

    Primer commit

---
+++ projects/xReal/src/TwitterFollowCard.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function TwitterFollowCard({ userName = "Sin Nombre", children, isFollowing, initialIsFollowing  }) {
+    // EL CHILDREN ES HIJO DE TwitterFollowCard EN APP.JSX
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+    
+    const text = isFollowing ? 'Siguiendo' : 'Seguir'
+  const buttonClassName = isFollowing
+    ? 'tw-followCard-button is-following'
+    : 'tw-followCard-button'
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header '>
+                <img
+                    className='tw-followCard-avatar '
+                    alt="Sin foto" 
+                    src={`https://unavatar.io/x/${userName}`}
+                />
+                <div className='tw-followCard-info'>
+                    <strong>{children}</strong>
+                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button onClick={handleClick} className={buttonClassName}>
+                <span className='tw-followCard-text'>{text}</span>
+                <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
+                </button>
+            </aside>
+        </article>
+    )
+}
