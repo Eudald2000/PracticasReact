fafb1c2ef84bb59e7d022b60970067981712c4f8
Eudald2000 <eudald08210@gmail.com>
2024-10-01 18:09:25 +0200

    localStorage (Finalizado)

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -3,10 +3,21 @@ import confetti from 'canvas-confetti'
 import { Square } from "./components/Square"
 import { TURNS, WINNER_COMBOS } from "./constants"
 import { WinnerModal } from "./components/WinnerModal"
+import { saveGameToStorage, resetGameStorage } from "./logic/storage"
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(() => {
+    const boardFromStorage = window.localStorage.getItem('board')
+    if(boardFromStorage) return JSON.parse(boardFromStorage)
+      return Array(9).fill(null)
+  })
+
+  const [turn, setTurn] = useState(() => {
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.X
+    // ?? Esto mira si es null o undefined
+    // || Esto mira si es false
+  })
   const [winner, setWinner] = useState(null)
 
   const checkWinner = (boardToCheck) => {
@@ -24,6 +35,8 @@ function App() {
     setBoard(Array(9).fill(null))
     setTurn(TURNS.X)
     setWinner(null)
+
+    resetGameStorage()
   }
 
   const checkEndGame = (newBoard) => {
@@ -34,12 +47,15 @@ function App() {
     const newBoard = [...board]
     //let newTurn = turn;
 
+    //Revisar casilla vacia o ganador
     if (board[index] || winner) {
       return
     }
+    // Actualizar tablero
     newBoard[index] = turn
     setBoard(newBoard)
 
+    // Cambiar turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     // SON LO MISMO
     // if(turn == TURNS.X){
@@ -49,6 +65,13 @@ function App() {
     // }
     setTurn(newTurn)
 
+    //Guardar partida
+    saveGameToStorage({
+      board: newBoard,
+      turn: newTurn
+    })
+
+    //Revisar si hay ganador
     const newWinner = checkWinner(newBoard)
     if (newWinner) {
       confetti()
@@ -62,8 +85,8 @@ function App() {
     <main className="board">
       <h1>TIC TAC TOE</h1>
       <button onClick={resetGame}>
-                EMPEZAR DE NUEVO
-              </button>
+        EMPEZAR DE NUEVO
+      </button>
       <section className="game">
         {
           board.map((_, index) => {
