b869ca6265cf3ad517842458f7446de494f42bbb
Eudald2000 <eudald08210@gmail.com>
2024-10-03 19:13:26 +0200

    3 proyecto finalizado

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,14 +1,79 @@
 import './App.css'
-import { useEffect } from "react"
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [color, setColor] = useState('rgba(0, 0, 0, 0.5)')
+  const [time, setTime] = useState(0)
+
+  const changeColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setColor(randomColor);
+  }
+
   useEffect(() => {
-    console.log('efecto')
-  })
+    let timer;
+
+    if(enabled){
+      timer = setInterval(() => {
+        setTime(prevTime => prevTime + 1);
+      }, 1000);
+    }
+
+    return() => {
+      clearInterval(timer);
+      setTime(0)
+    }
+
+  },[enabled])
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    //clean (se ejecuta cuando se desmonta el componente o cambian las dependencias)
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+
+  }, [enabled])
+
+
   return (
-    <>
-      <h3>Projecto 3</h3>
-    </>
+    <main>
+      {enabled &&
+        <div style={{
+          position: 'absolute',
+          backgroundColor: `${color}`,
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px`
+        }}>
+        </div>
+      }
+      <button onClick={() => { setEnabled(!enabled) }}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir el puntero
+      </button>
+      {
+        enabled &&
+        <button onClick={changeColor}>Cambiar color aleatorio</button>
+      }
+
+      <p>Tiempo transcurrido con el puntero activo: {time} segundos</p>
+    </main>
   )
 }
 
