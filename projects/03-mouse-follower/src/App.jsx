c3983dd5c000693d7a4ad19bad3c8800b0b0822f
Eudald2000 <eudald08210@gmail.com>
2024-10-03 17:02:00 +0200

    gitignore Inicio 3 proyecto

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useEffect } from "react"
+
+function App() {
+  useEffect(() => {
+    console.log('efecto')
+  })
+  return (
+    <>
+      <h3>Projecto 3</h3>
+    </>
+  )
+}
+
+export default App
