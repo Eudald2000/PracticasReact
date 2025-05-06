cd674d9f585100e3c325a4b12ceea622079b26df
Eudald2000 <eudald08210@gmail.com>
2025-05-06 13:12:38 +0200

    possible cagada

--- projects/02-tres-en-raya/src/App.jsx
+++ projects/02-tres-en-raya/src/App.jsx
@@ -6,8 +6,21 @@ import { PLAYER_TURNS, WINNER_COMBO } from "./constants";
 import { WinnerModal } from "./components/WinnerModal";
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [currentTurn, setCurrentTurn] = useState(PLAYER_TURNS.X);
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = JSON.parse(
+      window.localStorage.getItem("board")
+    );
+    if (boardFromLocalStorage) {
+      return boardFromLocalStorage;
+    }
+    return Array(9).fill(null);
+  });
+  
+  const [currentTurn, setCurrentTurn] = useState(() => {
+    const turnFromLocalStorage = window.localStorage.getItem("turn");
+    if (turnFromLocalStorage) return turnFromLocalStorage;
+    return PLAYER_TURNS.X;
+  });
   const [winner, setWinner] = useState(null);
 
   //comprueba si hay ganador segun los patrones de winnwer_combo
@@ -34,7 +47,7 @@ function App() {
     return true; // Si no hay casillas vacías, el juego ha terminado
   };
 
-  const handleSquareClick = (index) => {
+  const updateBoard = (index) => {
     // si ya hay figura sale o ganador
     if (board[index] || winner) return;
 
@@ -52,23 +65,29 @@ function App() {
       const newTurn =
         currentTurn === PLAYER_TURNS.X ? PLAYER_TURNS.O : PLAYER_TURNS.X;
       setCurrentTurn(newTurn);
+      // Guardar turno
+      window.localStorage.setItem("turn", newTurn);
     }
+    // Guardar tablero
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
   };
 
   const restartGame = () => {
     setBoard(Array(9).fill(null));
     setCurrentTurn(PLAYER_TURNS.X);
     setWinner(null);
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   };
 
   return (
     <main className="board">
       <h1>3 en raya</h1>
-
+      <button onClick={restartGame}>New Game</button>
       <section className="game">
         {board.map((square, index) => {
           return (
-            <Square key={index} index={index} onSquareClick={handleSquareClick}>
+            <Square key={index} index={index} onSquareClick={updateBoard}>
               {square}
             </Square>
           );
