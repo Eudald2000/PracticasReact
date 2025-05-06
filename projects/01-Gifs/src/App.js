a160eea476c904750032725b8f12247cfa8698d7
Eudald2000 <eudald08210@gmail.com>
2025-05-06 13:12:58 +0200

    possible cagada

--- projects/01-Gifs/src/App.js
+++ projects/01-Gifs/src/App.js
@@ -1,5 +1,5 @@
 
-import React, { use, useState } from 'react';
+import React, { use, useState } from "react";
 import './App.css';
 import ListOfGifs from './components/ListOfGifs';
 import Buscador from './components/buscador';
